d6fd791fa9d0ede45ca27410337cfd9114a3083c
susannabolz <109063938+susannabolz@users.noreply.github.com>
2026-03-19 16:00:54 +0000

    Deploying to gh-pages from @ susannabolz/susannabolz.github.io@88da2a7d6354272d5a02c937cd034c4e3b4668e8 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "susannabolz/subo.github.io",
+    "data-repo": "susannabolz/susannabolz.github.io",
     "data-repo-id": "",
     "data-category": "",
     "data-category-id": "",
